5db185d3a06b33062732243e75f17fc5f45a9e8a
Salla Koskinen <sallakos@mbnet.fi>
2019-11-06 11:17:33 +0200

    3.3 puhelinluettelon backend step3 valmis

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  }
+  else {
+    res.status(404).end()
+  }
+
+})
+
 app.get('/info', (req, res) => {
   const numberOfContacts = persons.length
   res.send(`<p>Phonebook has info for ${numberOfContacts} people</p>
